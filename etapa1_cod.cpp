d57dd268e45fa38ab1bc239d528b99fde23256ba
kethlynsara <kethlynsaraa@gmail.com>
2025-04-11 03:41:12 -0300

    Fix reading instance data

--- etapa1_cod.cpp
+++ etapa1_cod.cpp
@@ -125,32 +125,54 @@ public:
         istringstream iss(linha);
         string token;
 
+        cout << "Processando linha: [" << linha << "] na secao: [" << secao << "]" << endl;
+
         if (secao == "Nodes") {
-            int id;
-            if (iss >> id) vertices.insert(id);
+            // Formato: N1, N2, etc. ou apenas números
+            string node_id;
+            if (iss >> node_id) {
+                int id = 0;
+                if (node_id[0] == 'N') {
+                    id = stoi(node_id.substr(1));
+                } else {
+                    id = stoi(node_id);
+                }
+                vertices.insert(id);
+            }
         }
         else if (secao == "Required N") {
+            // Formato: N4    1    1
             string node_id;
             int demand, service_cost;
             if (iss >> node_id >> demand >> service_cost) {
-                int id = node_id[0] == 'N' ? stoi(node_id.substr(1)) : stoi(node_id);
+                int id = 0;
+                if (node_id[0] == 'N') {
+                    id = stoi(node_id.substr(1));
+                } else {
+                    id = stoi(node_id);
+                }
                 vertices_requeridos.insert(id);
                 vertices.insert(id);
             }
         }
-        else if (secao == "Edges" || secao == "EDGE") {
+        else if (secao == "Edges") {
+            // Formato: E1    2    3    18    1    19
             string edge_id, from_str, to_str;
             Aresta e;
-            if (secao == "Edges") {
-                if (iss >> edge_id >> from_str >> to_str >> e.traversal_cost >> e.demand >> e.service_cost) {
-                    e.from = from_str[0] == 'N' ? stoi(from_str.substr(1)) : stoi(from_str);
-                    e.to = to_str[0] == 'N' ? stoi(to_str.substr(1)) : stoi(to_str);
-                    e.required = true;
-                    arestas.push_back(e);
-                    vertices.insert(e.from);
-                    vertices.insert(e.to);
-                }
-            } else { // EDGE (não requerido)
+            if (iss >> edge_id >> from_str >> to_str >> e.traversal_cost >> e.demand >> e.service_cost) {
+                e.from = from_str[0] == 'N' ? stoi(from_str.substr(1)) : stoi(from_str);
+                e.to = to_str[0] == 'N' ? stoi(to_str.substr(1)) : stoi(to_str);
+                e.required = true;
+                arestas.push_back(e);
+                vertices.insert(e.from);
+                vertices.insert(e.to);
+            }
+        }
+        else if (secao == "EDGE") {
+            // Formato: EDGE FROM N. TO N. T. COST (cabeçalho) ou linhas de dados
+            if (linha.find("FROM N.") == string::npos) { // Se não for cabeçalho
+                string edge_id, from_str, to_str;
+                Aresta e;
                 if (iss >> edge_id >> from_str >> to_str >> e.traversal_cost) {
                     e.from = from_str[0] == 'N' ? stoi(from_str.substr(1)) : stoi(from_str);
                     e.to = to_str[0] == 'N' ? stoi(to_str.substr(1)) : stoi(to_str);
@@ -162,19 +184,24 @@ public:
                 }
             }
         }
-        else if (secao == "Arcs" || secao == "ARC") {
+        else if (secao == "Arcs") {
+            // Formato: A1    1    2    13    1    14
             string arc_id, from_str, to_str;
             Arco a;
-            if (secao == "Arcs") {
-                if (iss >> arc_id >> from_str >> to_str >> a.traversal_cost >> a.demand >> a.service_cost) {
-                    a.from = from_str[0] == 'N' ? stoi(from_str.substr(1)) : stoi(from_str);
-                    a.to = to_str[0] == 'N' ? stoi(to_str.substr(1)) : stoi(to_str);
-                    a.required = true;
-                    arcos.push_back(a);
-                    vertices.insert(a.from);
-                    vertices.insert(a.to);
-                }
-            } else { // ARC (não requerido)
+            if (iss >> arc_id >> from_str >> to_str >> a.traversal_cost >> a.demand >> a.service_cost) {
+                a.from = from_str[0] == 'N' ? stoi(from_str.substr(1)) : stoi(from_str);
+                a.to = to_str[0] == 'N' ? stoi(to_str.substr(1)) : stoi(to_str);
+                a.required = true;
+                arcos.push_back(a);
+                vertices.insert(a.from);
+                vertices.insert(a.to);
+            }
+        }
+        else if (secao == "ARC") {
+            // Formato: ARC FROM N. TO N. T. COST (cabeçalho) ou linhas de dados
+            if (linha.find("FROM N.") == string::npos) { // Se não for cabeçalho
+                string arc_id, from_str, to_str;
+                Arco a;
                 if (iss >> arc_id >> from_str >> to_str >> a.traversal_cost) {
                     a.from = from_str[0] == 'N' ? stoi(from_str.substr(1)) : stoi(from_str);
                     a.to = to_str[0] == 'N' ? stoi(to_str.substr(1)) : stoi(to_str);
@@ -189,10 +216,10 @@ public:
     }
 };
 
-// [Restante do código permanece igual...]
-
 void processarInstancia(const string& nomeArquivo, Grafo& grafo) {
-    ifstream arquivo(nomeArquivo);
+    string caminhoArquivo = "../instancias/" + nomeArquivo;
+    cout << "Abrindo arquivo: " << caminhoArquivo << endl;
+    ifstream arquivo(caminhoArquivo);
     if (!arquivo) {
         cerr << "Erro ao abrir o arquivo!\n";
         return;
@@ -200,24 +227,68 @@ void processarInstancia(const string& nomeArquivo, Grafo& grafo) {
 
     string linha;
     string secao;
+    bool headerProcessed = false;
     
-    // Pular o cabeçalho inicial
     while (getline(arquivo, linha)) {
-        cout << linha << endl; // DEBUG
-        if (linha.find("Name") != string::npos) {
-            break;
-        }
-    }
-    
-    // Processar as seções
-    while (getline(arquivo, linha)) {
+        // Remover espaços em branco no início e fim
+        linha.erase(0, linha.find_first_not_of(" \t"));
+        linha.erase(linha.find_last_not_of(" \t") + 1);
+        
         if (linha.empty()) continue;
         
-        if (linha[0] == '#') {
-            secao = linha.substr(1);
+        // Processar o cabeçalho inicial
+        if (!headerProcessed) {
+            if (linha.find("Name:") != string::npos) {
+                headerProcessed = true;
+            }
             continue;
         }
         
+        // Identificar seções
+        if (linha[0] == '#') {
+            if (linha.find("#Nodes") != string::npos) {
+                secao = "Nodes";
+            }
+            else if (linha.find("#Required N") != string::npos) {
+                secao = "Required N";
+            }
+            else if (linha.find("#Required E") != string::npos) {
+                secao = "Edges";
+            }
+            else if (linha.find("#Required A") != string::npos) {
+                secao = "Arcs";
+            }
+            else if (linha.find("#Edges") != string::npos) {
+                secao = "EDGE";
+            }
+            else if (linha.find("#Arcs") != string::npos) {
+                secao = "ARC";
+            }
+            continue;
+        }
+        else if (linha.find("ReN.") != string::npos) {
+            secao = "Required N";
+            continue;
+        }
+        else if (linha.find("ReE.") != string::npos) {
+            secao = "Edges";
+            continue;
+        }
+        else if (linha.find("ReA.") != string::npos) {
+            secao = "Arcs";
+            continue;
+        }
+        else if (linha.find("EDGE") != string::npos) {
+            secao = "EDGE";
+            continue;
+        }
+        else if (linha.find("ARC") != string::npos) {
+            secao = "ARC";
+            continue;
+        }
+        
+        // Processar linhas de dados
+        cout << "Processando linha: [" << linha << "] na secao: [" << secao << "]" << endl;
         grafo.processarLinha(linha, secao);
     }
     
@@ -227,24 +298,21 @@ void processarInstancia(const string& nomeArquivo, Grafo& grafo) {
 int main() {
     Grafo grafo;
     string nomeArquivo;
-    
     // Lista automaticamente os arquivos .dat
-    //listarArquivosDat();
-    
+    // listarArquivosDat();
+
     cout << "\nDigite o nome da instancia: ";
     getline(cin, nomeArquivo);
 
-    //if (!arquivoExiste(nomeArquivo)) {
-       // cerr << "\nERRO: Arquivo '" << nomeArquivo << "' nao encontrado!\n";
-       // listarArquivosDat();
-        //return 1;
+    // if (!arquivoExiste(nomeArquivo)) {
+    //  cerr << "\nERRO: Arquivo '" << nomeArquivo << "' nao encontrado!\n";
+    //  listarArquivosDat();
+    // return 1;
     //}
-
     cout << "\n=== Processando: " << nomeArquivo << " ===\n";
 
     processarInstancia(nomeArquivo, grafo);
 
-    // Exibindo as métricas
     cout << "\n=== Metricas da Instancia ===\n";
     cout << "1. Quantidade de vertices: " << grafo.qtdVertices() << endl;
     cout << "2. Quantidade de arestas: " << grafo.qtdArestas() << endl;
